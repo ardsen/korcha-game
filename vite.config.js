d2d0fdf2a90a974a213cc8c749c9e60e5a9581ac
Arda <as.ardasen@gmai.com>
2024-06-07 14:45:01 +0300

    Fix MIME type issues for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   base: '/',
-  plugins: [react(), eslint()],
   build: {
-    outDir: "build",
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   },
+  plugins: [react()],
 });
