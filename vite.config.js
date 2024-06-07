29f9925d1c14b5aa16b6cfb4c5fb6c05162d65e7
Arda <as.ardasen@gmai.com>
2024-06-07 16:44:46 +0300

    fw

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
   base: './korcha-game/',
+  plugins: [react()],
 });
